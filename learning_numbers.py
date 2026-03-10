8f34294ccc8d9071a015711534d2420aa1bb4e2a
himanigupta <himanigupta2902@gmail.com>
2026-03-10 12:39:21 +0530

    learning numbers and variable assignments

--- learning_numbers.py
+++ learning_numbers.py
@@ -6,3 +6,9 @@ print(a+a)
 a=30.1
 print(a)
 print((type(a)))
+my_income=10000
+my_tax_rate = 0.30
+my_tax = my_tax_rate*my_income
+print(my_tax)
+my_net_income = my_income-my_tax
+print(my_net_income)
