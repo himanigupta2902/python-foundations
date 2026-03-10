e9396bb41476d2b2fa3edcc21acebf2f8d9cc848
himanigupta <himanigupta2902@gmail.com>
2026-03-10 13:09:08 +0530

    working with strings

---
+++ working_with_strings.py
@@ -0,0 +1,24 @@
+print("hello")
+print('hello World')
+print("I'm a girl")
+print('I read books \n Sapiens \n Song of Ice and Fire \n\t House of Dragons')
+print(len('hello'))
+print(len("i'm a girl"))
+
+### String Slicing and Indexing ###
+
+mystring="Hello World"
+print(mystring)
+print(mystring[0])
+print(mystring[6])
+print(mystring[-1])
+print(mystring[-10])
+print(mystring[3:])
+print(mystring[:7])
+print(mystring[3:7])
+print(mystring[5:-1])
+print(mystring[5:])
+print(mystring[::2])
+print((mystring[2:10:2]))
+print(mystring[::-1])
+print(mystring[10:2:-2])
