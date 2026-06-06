d710bda5a514758ff8fae5b116d8c6dae86185ae
himanigupta <himanigupta2902@gmail.com>
2026-06-06 14:23:14 +0530

    Practicing inputs

--- sample_first_.py
+++ sample_first_.py
@@ -1,3 +1,12 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 print(f"Hello {name}, you will be {age + 1} next year!")
+
+euFloor = input("Enter Europe floor number : ")
+usFloor = int(euFloor)+1
+print(f"You are on {usFloor} floor")
+
+hours = int(input("Enter hours : "))
+rate = float(input("Enter rate :"))
+pay = hours*rate
+print("Your total pay is",pay)
